833839216842e37618f4a6fbffde90577dc095ff
ashxtt <111319560+ashxtt@users.noreply.github.com>
2023-02-04 11:43:34 -0800

    create a blue screen

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # snake-game-practice
+### Learning to create games using python by making a simple snake game
---
+++ snake_game.py
@@ -0,0 +1,7 @@
+import turtle 
+
+wn = turtle.Screen()
+wn.title("Snake Game by @lilgreyya")
+wn.bgcolor("blue")
+wn.setup(width=600, height=600)
+wn.tracer(0)
