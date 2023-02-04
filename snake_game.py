b1c0421c2a25730218abcc6a536dc9940b3335fe
ashxtt <111319560+ashxtt@users.noreply.github.com>
2023-02-04 12:29:21 -0800

    changed backgrounf color to black, got snake head to move off screen, set time delay to .1s

--- snake_game.py
+++ snake_game.py
@@ -1,7 +1,35 @@
 import turtle 
+import time
+
+delay = 0.1
 
 wn = turtle.Screen()
 wn.title("Snake Game by @lilgreyya")
-wn.bgcolor("blue")
+wn.bgcolor("black")
 wn.setup(width=600, height=600)
+# * turns off screen updates
 wn.tracer(0)
+
+head = turtle.Turtle()
+# *animation speed of the turtle
+head.speed(0)
+head.shape("square")
+head.color("red")
+# * pen up (not drawing)
+head.penup()
+head.goto(0,0)
+head.direction = "up"
+
+# * function to move my head up
+def move():
+    if head.direction == "up":
+        y = head.ycor()
+        head.sety(y + 20)
+
+# * This loop updates the screen
+while True:
+    wn.update()
+    move()
+    time.sleep(delay)
+
+wn.mainloop()
