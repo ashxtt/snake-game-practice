2b978c9380390bc7830a238fa2f3c256c2077016
ashxtt <111319560+ashxtt@users.noreply.github.com>
2023-02-04 12:47:02 -0800

    able to control my sneak head using keyboard(w,s,a,d)

--- snake_game.py
+++ snake_game.py
@@ -18,13 +18,42 @@ head.color("red")
 # * pen up (not drawing)
 head.penup()
 head.goto(0,0)
-head.direction = "up"
+head.direction = "stop"
 
 # * function to move my head up
+def go_up():
+    head.direction = "up"
+def go_left():
+    head.direction = "left"
+def go_right():
+    head.direction = "right"
+def go_down():
+    head.direction = "down"
+
 def move():
     if head.direction == "up":
         y = head.ycor()
         head.sety(y + 20)
+
+    if head.direction == "right":
+        x = head.xcor()
+        head.setx(x + 20)
+    
+    if head.direction == "left":
+        x = head.xcor()
+        head.setx(x - 20)
+    
+    if head.direction == "down":
+        y = head.ycor()
+        head.sety(y - 20)
+
+# * Keyword bindings
+# * Connects a keypress with a puticular function
+wn.listen()
+wn.onkeypress(go_up, "w")
+wn.onkeypress(go_left, "a")
+wn.onkeypress(go_right, "d")
+wn.onkeypress(go_down, "s")
 
 # * This loop updates the screen
 while True:
