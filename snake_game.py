92911848c5790c2b0751e74263f242b6dfe64892
ashxtt <111319560+ashxtt@users.noreply.github.com>
2023-02-04 15:38:32 -0800

    created segments to follow the snake head. able to grow when I eat food

--- snake_game.py
+++ snake_game.py
@@ -25,9 +25,11 @@ head.direction = "stop"
 food = turtle.Turtle()
 food.speed(0)
 food.shape("circle")
-food.color("brown")
+food.color("green")
 food.penup()
 food.goto(0,100)
+
+segments = []
 
 
 # * function to move my head up
@@ -68,10 +70,31 @@ wn.onkeypress(go_down, "s")
 # * This loop updates the screen
 while True:
     wn.update()
+    # * collison with the food
     if head.distance(food) < 20:
         x = random.randint(-290,290)
         y = random.randint(-290, 290)
         food.goto(x,y)
+
+        # * add segments
+        new_segment = turtle.Turtle()
+        new_segment.speed(0)
+        new_segment.shape("square")
+        new_segment.color("white")
+        new_segment.penup()
+        segments.append(new_segment)
+
+    # * MOve the end segments first 
+    for index in range(len(segments)-1, 0, -1):
+        x = segments[index-1].xcor()
+        y = segments[index-1].ycor()
+        segments[index].goto(x,y)
+
+    if len(segments) > 0:
+        x = head.xcor()
+        y = head.ycor()
+        segments[0].goto(x,y)
+
     move()
     time.sleep(delay)
 
